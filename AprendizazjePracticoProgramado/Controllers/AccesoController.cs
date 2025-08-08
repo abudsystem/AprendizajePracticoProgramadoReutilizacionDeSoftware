426b23dbe58edeffc9619db51c625ff3ef0623b2
abudsystem <abudsystem@gmail.com>
2025-08-08 14:00:48 -0500

    Configuraciones exactas hasta la clase del 7/08/2025

---
+++ AprendizazjePracticoProgramado/Controllers/AccesoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//importing the necessary libreries
+using System.Data;
+using System.Data.SqlClient;
+
+// importar funcionalidades
+using System.Security.Cryptography;
+using System.Configuration; 
+
+namespace AprendizazjePracticoProgramado.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autenticar()
+        {
+            return View();
+        }
+
+        // POST: Acceso/Autenticar
+        // [HttpPost]
+        //public ActionResult Autenticar()
+
+
+
+    }
+}
---
+++ AprendizazjePracticoProgramado/Models/Empleado.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AprendizazjePracticoProgramado.Models
+{
+    public class Empleado
+    {
+           public string cedula
+        {
+            get; set;
+        }
+
+        public string birth_date
+        {
+            get; set;
+        }
+        public string nombre
+        {
+            get; set;
+        }
+        public string apellido
+        {
+            get; set;
+        }
+        public string genero
+        {
+            get; set;
+        }
+        public string hire_date
+        {
+            get; set;
+        }
+        public string correo
+        {
+            get; set;
+        }
+        public string usuario
+        {
+            get; set;
+        }
+        public string clave
+        {
+            get; set;
+        }
+        public string Id
+        {
+            get; set;
+        }
+    }
+}
