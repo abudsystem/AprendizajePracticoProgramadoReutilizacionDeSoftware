8c8bd11779ab1b82dc6abbf76c8b59004ce6486d
Erik Andres <erikqtrs@gmail.com>
2025-08-15 13:27:25 -0500

    Agregado modulo de registro de empleado

--- AprendizazjePracticoProgramado/Controllers/EmpleadoController.cs
+++ AprendizazjePracticoProgramado/Controllers/EmpleadoController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using AprendizazjePracticoProgramado.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +17,36 @@ namespace AprendizazjePracticoProgramado.Controllers
         public ActionResult Registrar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar( Empleado emp )
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Cnn"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_insertEmployee", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("ci", emp.ci);
+                    cmd.Parameters.AddWithValue("nombre", emp.nombre);
+                    cmd.Parameters.AddWithValue("apellido", emp.apellido);
+                    cmd.Parameters.AddWithValue("fechaNacimiento", emp.fechaNacimiento);
+                    cmd.Parameters.AddWithValue("genero", emp.genero);
+                    cmd.Parameters.AddWithValue("correo", emp.correo);
+                    cmd.Parameters.AddWithValue("clave", emp.clave);
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex);
+                System.Diagnostics.Debug.WriteLine("Error: " + ex.Message);
+                return View();
+            }
         }
     }
 }
