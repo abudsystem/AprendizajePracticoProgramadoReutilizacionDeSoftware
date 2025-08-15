7276b382170916801fefe8969fccc7ec1efba6d5
Erik Andres <erikqtrs@gmail.com>
2025-08-15 12:39:28 -0500

    Creacion del controlador Empleado

---
+++ AprendizazjePracticoProgramado/Controllers/EmpleadoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AprendizazjePracticoProgramado.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // GET: Empleado
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
